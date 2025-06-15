5043114d42d39d6ab2fa53049963b88109c38efb
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2025-06-14 23:34:32 -0400

    chore: format main.tf for consistency and readability

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,12 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_droplet" "web" {
-  name   = "react-web-01"
-  region = "nyc1"
-  size   = "s-2vcpu-2gb"
-  image  = "ubuntu-24-04-x64"
+  name     = "react-web-01"
+  region   = "nyc1"
+  size     = "s-2vcpu-2gb"
+  image    = "ubuntu-24-04-x64"
   ssh_keys = [var.ssh_fingerprint]
-  tags   = ["react-app"]
+  tags     = ["react-app"]
 }
 
 output "ip_address" {
