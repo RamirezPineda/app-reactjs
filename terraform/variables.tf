d00de8ba77510462acb5e6bc9d59d7d9a62934b7
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2025-06-14 23:23:32 -0400

    feat: add variables terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "do_token" {
+  type        = string
+  description = "Token API of DigitaloCean"
+}
+variable "ssh_fingerprint" {
+  type        = string
+  description = "Authorized SSH key"
+}
